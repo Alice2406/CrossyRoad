6aa3592eb64c2ad589c6def933f3de0b07954809
hbelaskri <hbelaskri@gaming.tech>
2026-03-11 13:01:58 +0100

    debut scene

---
+++ FiniteStateMachine/Scene.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class Scene
+{
+public:
+	virtual ~Scene() = default;
+
+	virtual void handleInput(sf::RenderWindow& Window) = 0;
+	virtual void update(float dt) = 0;
+	virtual void draw(sf::RenderWindow& Window) = 0;
+	
+};
+
