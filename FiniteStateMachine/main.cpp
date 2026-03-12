880a1355155c837a5bfaf785f76d4afcf39563ae
unknown <hbelaskri@gamingcampus.local>
2026-03-12 10:54:39 +0100

    skinscene

--- FiniteStateMachine/main.cpp
+++ FiniteStateMachine/main.cpp
@@ -1,34 +1,43 @@
 #include <SFML/Graphics.hpp>
 #include "MenuScene.h"
+#include "SkinScene.h"
 #include <memory>
-#include <optional>
 
 int main() {
-  
-    sf::RenderWindow window(sf::VideoMode({ 1800, 900 }), "Crossy Road");
+    sf::RenderWindow window(sf::VideoMode({ 1800, 900 }), "Crossy Road SFML 3.2");
     window.setFramerateLimit(60);
 
     std::unique_ptr<Scene> currentScene = std::make_unique<MenuScene>();
     sf::Clock clock;
 
     while (window.isOpen()) {
-    
         float dt = clock.restart().asSeconds();
-       
+
+    
         currentScene->handleInput(window);
-       
+
+ 
         while (const std::optional event = window.pollEvent()) {
             if (event->is<sf::Event::Closed>()) {
                 window.close();
             }
         }
-//        gameScene.update(dt, window);
+
+        if (MenuScene* menu = dynamic_cast<MenuScene*>(currentScene.get())) {
+            if (menu->goToSkin) {
+                currentScene = std::make_unique<SkinScene>();
+            }
+        }
+        else if (SkinScene* skin = dynamic_cast<SkinScene*>(currentScene.get())) {
+            if (skin->backToMenu) {
+                currentScene = std::make_unique<MenuScene>();
+            }
+        }
+
         currentScene->update(dt, window);
 
-        window.clear(sf::Color::White); 
-
+        window.clear(sf::Color::White);
         currentScene->draw(window);
-
         window.display();
     }
     return 0;
