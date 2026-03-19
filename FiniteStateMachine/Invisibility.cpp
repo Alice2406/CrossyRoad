9a06bf12291c153685f40f73c3bfad825b71055f
hbelaskri <hbelaskri@gaming.tech>
2026-03-19 17:22:14 +0100

    ajout de l'effet de mort

--- FiniteStateMachine/Invisibility.cpp
+++ FiniteStateMachine/Invisibility.cpp
@@ -4,5 +4,5 @@
 
 void Invisibility::applyEffect(Player& player) {
     std::cout << "Recolte : INVISIBILITE (5.0s)" << std::endl;
-    player.setGhostMode(5.0f); // On active le mode fantôme pour 5 secondes
+    player.setGhostMode(5.0f); 
 }
--- FiniteStateMachine/Player.h
+++ FiniteStateMachine/Player.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include "Map.h"
+
 class Player
 {
 public:
@@ -9,58 +10,45 @@ public:
     void update(float dt, Map& map);
     void draw(sf::RenderWindow& window);
     void spawn(const sf::Vector2f& position);
+
     sf::Vector2f getGridPos() const { return m_gridPos; }
+    void setGridPos(sf::Vector2f newPos) { m_gridPos = newPos; }
     sf::Vector2f getPosition() const;
     sf::Vector2f getIsoPosition() const;
     sf::FloatRect getGridBounds() const;
-    void setGridPos(sf::Vector2f newPos) { m_gridPos = newPos; }
 
-    void addLife() { if (m_lives < 2) m_lives++; } 
+  
+    void addLife() { if (m_lives < 2) m_lives++; }
     void loseLife() { m_lives--; }
     int getLives() const { return m_lives; }
 
-    void setInvisibility(float duration) { m_isInvisible = true; m_invisTimer = duration; }
+  
+    void setGhostMode(float duration) { m_ghostTimer = duration; }
+    void startShake(float duration) { m_shakeTimer = duration; }
 
-
-
-    void setGhostMode(float duration) {
-        m_isGhost = true;
-        m_ghostTimer = duration;
-    }
-
-    bool isGhost() const { return m_isGhost; }
-    bool isInvincible() const { return m_isInvincible; }
+   
     bool isInvisible() const { return m_ghostTimer > 0.f; }
-
-    float m_flashTimer = 0.f; 
-    void setInvincibleTimer(float duration) { m_invincibleTimer = duration; }
-
+    bool isShaking() const { return m_shakeTimer > 0.f; }
 
 private:
+  
     int m_currentFrame = 0;
     int m_frameCount = 9;
     float m_frameTime = 0.15f;
     float m_elapsedTime = 0.f;
     sf::Vector2i m_frameSize;
-    sf::Vector2f m_gridPos;
-    sf::Vector2f m_basePos;
-    sf::RectangleShape m_shape;
-    float m_step; 
-    bool m_moveLocked = false;
     sf::Texture m_texture;
     sf::Sprite m_sprite;
 
-    int m_lives = 1;             
-    bool m_isInvisible = false;
-    float m_invisTimer = 0.0f;  
+   
+    sf::Vector2f m_gridPos;
+    sf::Vector2f m_basePos;
+    sf::RectangleShape m_shape;
+    float m_step;
+    bool m_moveLocked = false;
 
-
-    bool m_isGhost = false;      
-    float m_ghostTimer = 0.0f;  
-    bool m_isInvincible = false;
-
-    float m_invincibleTimer = 0.f;
-    float m_invisibleTimer = 0.f;
-
+   
+    int m_lives = 1;          
+    float m_ghostTimer = 0.f; 
+    float m_shakeTimer = 0.f; 
 };
-
